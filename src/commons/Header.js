ecfb3bebf397811b0c72eda1c46bec814c2895ed
shaikhsmsuss <shaikh.shoeb96@gmail.com>
2022-10-31 01:52:22 +0530

    added route link to the header name spacex

--- src/commons/Header.js
+++ src/commons/Header.js
@@ -1,12 +1,15 @@
 import React from "react";
 import AppBar from "@mui/material/AppBar";
 import Toolbar from "@mui/material/Toolbar";
+import { Link } from "react-router-dom";
 
 function Header() {
   return (
     <AppBar position="fixed" className="Header">
       <Toolbar>
-        <div className="header-wrapper">SPACEX</div>
+        <Link to="/spacex" style={{ textDecoration: "none" }}>
+          <div className="header-wrapper">SPACEX</div>
+        </Link>
       </Toolbar>
     </AppBar>
   );
